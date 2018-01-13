700e95ac5b75e1293ae4eee1bbbdd0a69f796fcc
Pedro Rio <pedrorio@gmail.com>
2018-01-13 12:22:29 +0000

    Add ember-power-select

--- app/components/patient-form/component.js
+++ app/components/patient-form/component.js
@@ -1,7 +1,23 @@
 import Component from '@ember/component';
-import { set } from '@ember/object';
+import { get, set } from '@ember/object';
+
+const countries = [
+  { name: 'United States',  flagUrl: '/flags/us.svg' },
+  { name: 'Spain',          flagUrl: '/flags/es.svg' },
+  { name: 'Portugal',       flagUrl: '/flags/pt.svg' },
+  { name: 'Russia',         flagUrl: '/flags/ru.svg' },
+  { name: 'Latvia',         flagUrl: '/flags/lv.svg' },
+  { name: 'Brazil',         flagUrl: '/flags/br.svg' },
+  { name: 'United Kingdom', flagUrl: '/flags/gb.svg' },
+];
 
 export default Component.extend({
   // Reference to the current patient
-  patient: null
+  patient: null,
+
+  init() {
+    this._super(...arguments);
+    set(this, 'countries', countries);
+    set(this, 'location', countries[2]);
+  }
 });
